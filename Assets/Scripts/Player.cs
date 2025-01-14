02fe7c2283f6337a343c109ebabf807a2d92d59c
minjikim0915 <minjikim0915@gmail>
2025-01-13 17:04:51 -0800

    Playable

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,43 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        float moveWS = 0f;
+        float moveAD = 0f;
+
+        if (Input.GetKey(KeyCode.W)) moveWS = 1f;
+        if (Input.GetKey(KeyCode.A)) moveAD = -1f;
+        if (Input.GetKey(KeyCode.S)) moveWS = -1f;
+        if (Input.GetKey(KeyCode.D)) moveAD = 1f;
+
+        Vector2 movement = new Vector2(moveAD, moveWS).normalized;
+        movement *= _speed * Time.deltaTime;
+        _playerTransform.Translate(movement.x, movement.y, 0);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantSeed();
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        if (_numSeedsLeft == 0) return;
+
+
+        Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+
+        _numSeedsLeft--;
+        _numSeedsPlanted++;
+
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 }
